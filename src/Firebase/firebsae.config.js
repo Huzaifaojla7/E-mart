86e941fe137d6272a54e26f35017a3bafdafa7d5
Huzaifaojla7 <65033278+Huzaifaojla7@users.noreply.github.com>
2023-08-27 21:16:53 +0500

    Update firebsae.config.js

--- src/Firebase/firebsae.config.js
+++ src/Firebase/firebsae.config.js
@@ -5,6 +5,12 @@ import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
+    apiKey: "AIzaSyCNRq7UG_Bl9CC7NynNMEhng_DMu_UHwQw",
+  authDomain: "emart-e20dc.firebaseapp.com",
+  projectId: "emart-e20dc",
+  storageBucket: "emart-e20dc.appspot.com",
+  messagingSenderId: "275061015083",
+  appId: "1:275061015083:web:2527d41e98e181e8afa06e"
 };
 
 const app = initializeApp(firebaseConfig);
